7e4249057b50a93624d8afbc67055ba1a82ed53c
Ronak Karia <ronakkaria06@gmail.com>
2015-03-08 07:12:32 +0530

    socket test

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,35 @@
 package controllers;
 
 import play.*;
+import play.libs.F;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
+    }
+    
+    public static WebSocket<String> socket() {
+		return new WebSocket<String>() {
+
+			@Override
+			public void onReady(WebSocket.In<String> in,
+					final WebSocket.Out<String> out) {
+				in.onMessage(new F.Callback<String>() {
+
+					@Override
+					public void invoke(String event) throws Throwable {
+						out.write("Just checking bitch");
+						Logger.info(event);
+					}
+				});
+
+				out.write("Freeze Motherfucker");
+			}
+
+		};
     }
 
 }
