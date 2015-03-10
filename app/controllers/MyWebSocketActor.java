a427ef93a4c8c902763cad198510cab55eec8400
Ronak Karia <ronakkaria06@gmail.com>
2015-03-11 00:31:49 +0530

    splitting with semi colon

--- app/controllers/MyWebSocketActor.java
+++ app/controllers/MyWebSocketActor.java
@@ -24,12 +24,13 @@ public class MyWebSocketActor extends UntypedActor {
     public void onReceive(Object message) throws Exception {
         if (message instanceof String) {
             
-        	Integer count = 1 + Integer.parseInt(((String)message).substring(0, 1));
+        	String actualMessage = ((String) message).split(";")[0];
+        	Integer count = 1 + Integer.parseInt(actualMessage);
 			
         	out.tell("Just checking bitch " + count, self());
 			if (count > maxCount) {
 				maxCount = count;
-				Logger.info(((String)message).substring(0,1));
+				Logger.info(actualMessage);
 			}
         }
     }
